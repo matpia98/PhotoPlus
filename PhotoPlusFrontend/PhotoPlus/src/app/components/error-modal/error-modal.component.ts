0932b8ec5effdb19ed56ece9595882fbe0a74a42
mpiasecki <mpiasecki@griddynamics.com>
2025-03-18 10:52:17 +0100

    fix: error popup

--- PhotoPlusFrontend/PhotoPlus/src/app/components/error-modal/error-modal.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/error-modal/error-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -6,19 +6,36 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './error-modal.component.html',
   styleUrls: ['./error-modal.component.scss']
 })
-export class ErrorModalComponent implements OnInit {
+export class ErrorModalComponent implements OnInit, AfterViewInit {
 
   @Input() public message: string;
 
   @Input() public title: string;
 
+  @ViewChild('closeBtn') closeBtn: ElementRef;
+
   constructor(private activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
+    // Make sure the modal can be closed with Escape key
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape') {
+        this.close();
+      }
+    });
+  }
 
+  ngAfterViewInit(): void {
+    // Focus might help with accessibility
+    setTimeout(() => {
+      if (this.closeBtn) {
+        this.closeBtn.nativeElement.focus();
+      }
+    }, 100);
   }
 
   close() {
+    console.log('Close button clicked');
     this.activeModal.close();
   }
 
--- PhotoPlusFrontend/PhotoPlus/src/app/interceptors/error-interceptor.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/interceptors/error-interceptor.ts
@@ -48,24 +48,32 @@ export class ErrorInterceptor implements HttpInterceptor {
   }
 
   handleError(error: HttpErrorResponse) {
+    // Define modal options once
+    const modalOptions = {
+      backdrop: false,
+      keyboard: true,
+      centered: true,
+      windowClass: 'error-modal-window'
+    };
+
     switch (error.status) {
       case 0: {
         //only one modal even if more than 1 error to prevent modal spam
-        const modalRef = this.modalService.open(ErrorModalComponent);
+        const modalRef = this.modalService.open(ErrorModalComponent, modalOptions);
         modalRef.componentInstance.title = "Error occured!";
         modalRef.componentInstance.message = "No connection to backend.";
         break;
       }
       case 401: {
         //only one modal even if more than 1 error to prevent modal spam
-        const modalRef = this.modalService.open(ErrorModalComponent);
+        const modalRef = this.modalService.open(ErrorModalComponent, modalOptions);
         modalRef.componentInstance.title = "Error occured!";
         modalRef.componentInstance.message = error.error.error;
         break;
       }
       case 403: {
         //only one modal even if more than 1 error to prevent modal spam
-        const modalRef = this.modalService.open(ErrorModalComponent);
+        const modalRef = this.modalService.open(ErrorModalComponent, modalOptions);
         modalRef.componentInstance.title = "Error occured!";
         modalRef.componentInstance.message = "Access denied.";
         break;
@@ -74,13 +82,13 @@ export class ErrorInterceptor implements HttpInterceptor {
         if (Array.isArray(error.error)) {
           const errorArray = error.error as Array<ErrorModel>;
           errorArray.forEach(el => {
-            const modalRef = this.modalService.open(ErrorModalComponent);
+            const modalRef = this.modalService.open(ErrorModalComponent, modalOptions);
             modalRef.componentInstance.title = "Error occured!";
             modalRef.componentInstance.message = el.message;
           });
         } else {
           const err = error.error as ErrorModel;
-          const modalRef = this.modalService.open(ErrorModalComponent);
+          const modalRef = this.modalService.open(ErrorModalComponent, modalOptions);
           modalRef.componentInstance.title = "Error occured!";
           modalRef.componentInstance.message = err.message;
         }
